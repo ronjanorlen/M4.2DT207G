ab446186fe85342324892f1a9e52cf89ac507d66
Ronja Norlén <rono2300@student.miun.se>
2024-05-02 12:22:35 +0200

    Justerat js-fil för registrering, fortsatt fix med styling

--- src/js/register.js
+++ src/js/register.js
@@ -1,11 +1,13 @@
 "use strict";
 
-// Funktion för att skapa ny användare, exporteras
+/* Funktion för att skapa ny användare, exporteras */
 export async function createUser() {
     const url = "https://moment-4-1-backend.onrender.com/api/register";
     const username = document.getElementById("newUsername").value;
     const password = document.getElementById("newPassword").value;
     const successMessage = document.getElementById("successMessage");
+    const errormessage = document.getElementById("error-message");
+    
 
     const userData = {
         username: username,
@@ -26,9 +28,14 @@ export async function createUser() {
             successMessage.innerHTML = "Användare skapad! <br> Gå till <a href='index.html'>startsidan</a> för att logga in";
             successMessage.classList.add("show");
 
+            // Dölj felmeddelandet när båda fält är ifyllda och konto skapats
+            errormessage.style.display = "none";
+
         } else {
             // Något gick fel vid skapandet av användaren, visa felmeddelande
-            console.error("Något gick fel vid skapandet av användare");
+            errormessage.textContent = "Fyll i både användarnamn och lösenord";
+            errormessage.style.display = "block";
+            
         }
     } catch (error) {
         console.error("Något gick fel vid anropet till API:et: ", error);
